d523719a7a2225a28454f738bf78cd45a1f8796b
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2022-10-27 20:50:10 -0300

    agregamos el desafio numero 1 del curso

---
+++ Desafio-1/Desafio-1/app.js
@@ -0,0 +1,64 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+    getFullName() {
+        return `El nombre completo es: ${this.nombre} ${this.apellido}`
+    }
+
+
+    addMascota(nombreMascota) {
+      this.mascotas.push(nombreMascota)
+    }
+    
+    countMascotas() {
+        return `La cantidad de mascotas es: ${this.mascotas.length}`
+    }
+    
+    addBook(nombre, autor) { 
+        this.libros.push(nombre,autor)
+    }
+
+    getBookNames(libros) {
+        return `Los nombres de los libros son: ${libros.nombre}`
+    }
+
+}
+
+
+let persona1 = {
+    nombre: "Soel",
+    apellido: "Attala",
+    libros: [{
+        nombre: "Ergagon",
+        autor: "Cristopher Paolini",
+    },
+    ],
+
+    mascotas: ["Roco", "Coquito"],
+}
+
+let persona = new Usuario(persona1.nombre, persona1.apellido, persona1.libros, persona1.mascotas)
+
+console.log(persona.getFullName())
+
+persona.addMascota("Lulu")
+persona.addMascota("Candy")
+console.log(persona.mascotas)
+
+console.log(persona.countMascotas())
+
+persona.addBook({nombre:"Las Cronicas de Narnia", autor: "CS Lewis"})
+console.log(persona.libros)
+
+
+console.log(persona.getBookNames)
+
+
+
+
+
+
